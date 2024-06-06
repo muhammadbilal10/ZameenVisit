bb678204a9300dfcfbbabad30e0472c46da9cd9d
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2024-06-07 02:57:52 +0500

    update feature card

--- src/components/property/PropertyList/PropertyFeaturesCard.tsx
+++ src/components/property/PropertyList/PropertyFeaturesCard.tsx
@@ -15,8 +15,8 @@ export default function PropertyFeaturesCard({ features }: { features: any }) {
             <div key={index}>
               <h3 className=" font-semibold my-4">{feature?.title}</h3>
               <ul className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
-                {feature?.items?.map((item: any) => (
-                  <li className="flex items-center">
+                {feature?.items?.map((item: any, idx: number) => (
+                  <li key={idx} className="flex items-center">
                     <CheckCircle className="text-[#26759b] w-4 h-4 mr-2" />
                     {item}
                   </li>
