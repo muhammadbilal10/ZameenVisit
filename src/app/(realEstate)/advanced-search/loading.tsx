3e63fa4c9bd82ac1d67547283b50d881dfc89a68
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2024-06-08 21:02:32 +0500

    update loading page

--- src/app/(realEstate)/advanced-search/loading.tsx
+++ src/app/(realEstate)/advanced-search/loading.tsx
@@ -10,11 +10,17 @@ export default function Loading() {
         <Skeleton className="h-4 w-[200px]" />
       </div>
 
-      <div className="mt-10">
-        <div className="space-y-1">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
+      <div className="gap-3 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 lg:grid-cols-3">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <Skeleton key={index} className="h-96" />
+        ))}
       </div>
     </div>
   );
